681ecd4d5f17cbb3b710571c98dc438198ce8fb1
aleksANJJ <meshkova8171@gmail.com>
2022-01-30 21:16:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [summerCamp, setSummerCamp] = useState(data);
+
+const removeBtn = (id) => {
+  let newSummerList = summerCamp.filter(sumCamp => sumCamp.id !== id);
+  setSummerCamp(newSummerList);
+  }
+
+  
+
+  return(
+    <div className='heading'>
+      <div className='container'>
+        <h1>USA {summerCamp.length} Summer Camp 2022</h1>
+      </div>
+      {summerCamp.map((summer => {
+        const {id, image, country, stat, campName, address, site, description, price} = summer;
+        
+        return (
+          <div key ={id}>
+            <div className='container'>
+              <h2>{id} - {campName}</h2>
+            </div>
+            <div className='container'>
+            <p>{country}, {stat}, {address}</p>
+            </div>
+            <div className='container'>
+              <img src={image} width="400px" alt="foto"/>
+            </div>
+            <div className='container'>
+              <p>{description}</p>
+            </div>
+            <div className='container'>
+              <p>{site}</p>
+            </div>
+            <div className='container'>
+              <h3>Price: {price}</h3>
+            </div>
+            <div className='btn'>
+              <button onClick={() => removeBtn(id)} className='red'>Remove</button>
+              <button className='blue'>Booking</button>
+            </div>
+          </div> )
+        }))}
+      <div className='container'>
+        <button onClick={() => setSummerCamp([])} className='red'>Delete All</button>
+      </div>
+     
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,49 @@
+
+
+
+export const data = [
+    {
+        id:1,
+        image: "https://avatars.mds.yandex.net/i?id=b7da533d6f0ddc7d1d59a25be9894fd7-5672854-images-thumbs&n=13", 
+        country: "USA",
+        stat: "Florida",
+        campName: "American Heritage Summer Camp",
+        address: "Broward County Campus, 12200 W. Broward Blvd, Plantation, FL 33325",
+        site: "https://online.ahschool.com",
+        description: "American Heritage Schools summer camp programs are designed for children ages 3-14. In addition to Day Camp, specialty camps vary by campus, and include Robotics, Art, Science, Musical Theatre, Video Production and Sports camps.",
+        price: "550$/week"
+    },
+    {
+        id:2,
+        image: "https://avatars.mds.yandex.net/i?id=17c5428dd9ff7532e8ebd7ab89f43fc9-5232914-images-thumbs&n=13",
+        country: "USA",
+        stat: "Massachusetts",
+        campName: "Camp Grossman",
+        address: "Boston 333 Nahanton Street, Newton, MA",
+        site: "www.bostonjcc.org",
+        description: "Situated on 75 acres in Dover’s pine-studded Hale Reservation, Camp Grossman is a co-ed Jewish day camp where summer days are filled with aquatics, sports, arts, nature, and more!  Activities provide opportunities to explore life outdoors, make lifelong friends, and develop new skills in a warm and positive Jewish camping community. Discover why our campers go gaga for Grossman every summer!",
+        price: "645$/week"
+    },
+    {
+        id:3,
+        image: "https://avatars.mds.yandex.net/i?id=5b70f6fc650b313b76e24d121d91ad9c-5219777-images-thumbs&n=13",
+        country: "USA",
+        stat: "Pennsylvania",
+        campName: "ABOUT CAMP IHC",
+        address: "3287 Hancock Hwy Equinunk, PA 18417",
+        site:"https://www.campihc.com/",
+        description: "Our mission at Camp IHC is committed to human development. Core values are modeled and learned in a nurturing community environment. Enthusiastic, skilled, and caring staff members facilitate the development of relationships, enhancing a positive self-image.",
+        price: "SUMMER SESSION TUITION 14650$"
+    },
+    {
+        id:4,
+        image: "https://avatars.mds.yandex.net/i?id=138c34bb23443ca067dae414ce0f6a5b-5305911-images-thumbs&n=13",
+        country: "USA",
+        stat: "California",
+        campName: "The Sunset Summer Day Camp",
+        address: "2550 25th Ave, San Francisco, CA 94116",
+        site:"https://steveandkatescamp.com",
+        description: "Every aspect of our program is designed to put kids in charge of their own learning experience. At Steve & Kate’s, campers choose from a variety of activities in real-time, as they design their day at camp. As kids try, fail, and learn on their own–they learn to trust their own judgment. These will ultimately be the kids who are prepared to tackle the challenges of an ever changing world with creativity and confidence.",
+        price: "109$ per day"
+    }
+]
